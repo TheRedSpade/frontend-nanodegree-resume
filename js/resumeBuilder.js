2ab8d0e56acd028c09b19c40929cdfe77ecb8949
Q Cox <qcox@redspade.software>
2016-08-20 11:23:57 -0400

    Added formatted name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
+var firstName = "Q";
+var lastName = "Cox";
+var name = firstName + " " + lastName;
+var awesomeThoughts = "I am " + name + " and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
